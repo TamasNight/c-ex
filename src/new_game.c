3cafa709f2c59f62f00c3c9368379bb9f138cd6f
TamasNight <tom2001valt@gmail.com>
2025-09-08 13:10:53 +0200

    unbound quest menu merge

--- src/new_game.c
+++ src/new_game.c
@@ -45,6 +45,7 @@
 #include "mystery_gift.h"
 #include "union_room_chat.h"
 #include "constants/map_groups.h"
+#include "quests.h"
 #include "constants/items.h"
 #include "difficulty.h"
 #include "follower_npc.h"
@@ -209,6 +210,7 @@ void NewGameInitData(void)
     WipeTrainerNameRecords();
     ResetTrainerHillResults();
     ResetContestLinkResults();
+    QuestMenu_ResetMenuSaveData();
     SetCurrentDifficultyLevel(DIFFICULTY_NORMAL);
     ResetItemFlags();
     ResetDexNav();
