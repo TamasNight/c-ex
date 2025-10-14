b8096d186dcfe1e983c051df6b685b3aa858cf24
TamasNight <tom2001valt@gmail.com>
2025-10-14 11:47:13 +0200

    pointer 4 auto_obj

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -405,6 +405,8 @@ $(OBJEVENTGFXDIR)/people/woman_5.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/youngster.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
+# Medieval - Tamas Class System
+
 $(OBJEVENTGFXDIR)/peoplemedieval/farmer_M.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
