83b20932a86383bcfbf8b5c51759720c3dcec2d1
TamasNight <tom2001valt@gmail.com>
2025-11-08 08:53:08 +0100

    Route 102 updates: adjusted berry trees, NPC positions, and connections; renamed related constants and flags; added new events and scripts for Valle Fredda integration. Updated layouts and graphics.

--- include/constants/berry.h
+++ include/constants/berry.h
@@ -44,9 +44,9 @@
 // IDs for berry tree objects, indexes into berryTrees in SaveBlock1
 // Named for whatever berry is initially planted there on a new game
 // Those with no initial berry are named "soil"
-#define BERRY_TREE_ROUTE_102_PECHA    1
-#define BERRY_TREE_ROUTE_102_ORAN     2
-#define BERRY_TREE_ROUTE_104_SOIL_1   3
+#define BERRY_TREE_VALLE_FREDDA_PECHA    1
+#define BERRY_TREE_VALLE_FREDDA_ORAN     2
+#define BERRY_TREE_VALLE_FREDDA_SOIL_1   3
 #define BERRY_TREE_ROUTE_104_ORAN_1   4
 #define BERRY_TREE_ROUTE_103_CHERI_1  5
 #define BERRY_TREE_ROUTE_103_LEPPA    6
